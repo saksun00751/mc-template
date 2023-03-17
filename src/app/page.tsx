8ff2c98bbd81bb1121f36f5959991c6187d74306
mckazine <mckazine@mckazines-Mac-mini.local>
2023-03-17 09:35:13 +0700

    Mc dev

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import styles from './page.module.css'
 import Nav from 'components/Nav'
 import Main from 'components/Main'
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
